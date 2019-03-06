930d358025e08d5c18037ab85e98c755f8d013b5
irina.scurtu <irina.scurtu@fortech.ro>
2019-03-06 17:12:46 +0200

    s

--- GraphQL.Api/GraphQL.Api/Data/Entities/Talk.cs
+++ GraphQL.Api/GraphQL.Api/Data/Entities/Talk.cs
@@ -7,6 +7,9 @@ namespace GraphQL.Api.Data.Entities
         public int Id { get; set; }
         public int SpeakerId { get; set; }
 
+        public string Description { get; set; }
+        public string Title { get; set; }
+
         [ForeignKey("SpeakerId")]
         [InverseProperty("Talks")]
         public virtual Speaker Speaker { get; set; }
--- GraphQL.Api/GraphQL.Api/Data/InitialSeed.cs
+++ GraphQL.Api/GraphQL.Api/Data/InitialSeed.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GraphQL.Api.Data.Entities;
 using GraphQL.Api.Data.Infrastructure;
+using System.Linq;
 
 namespace GraphQL.Api.Data
 {
@@ -12,6 +10,61 @@ namespace GraphQL.Api.Data
         {
             if (!dbContext.Speakers.Any())
             {
+                dbContext.Speakers.Add(new Speaker()
+                {
+                    CompanyName = "Edava",
+                    CompanyWebsite = "http://endava.com",
+                    Email = "irina@endava.com",
+                    FirstName = "Irina",
+                    LastName = "Scurtu",
+                    Position = "Design Lead",
+                    Description = "lalalaalala",
+                    PageSlug = "irina-scurtu"
+
+                });
+
+                dbContext.Speakers.Add(new Speaker()
+                {
+                    CompanyName = "Edava",
+                    CompanyWebsite = "http://endava.com",
+                    Email = "irina@endava.com",
+                    FirstName = "Irina",
+                    LastName = "Scurtu",
+                    Position = "Design Lead",
+                    Description = "lalalaalala",
+                    PageSlug = "irina-scurtu"
+
+                });
+
+                dbContext.Speakers.Add(new Speaker()
+                {
+                    CompanyName = "Edava",
+                    CompanyWebsite = "http://xyz.com",
+                    Email = "irina@xyz.com",
+                    FirstName = "Irina",
+                    LastName = "xyz",
+                    Position = "Devops",
+                    Description = "lalalaalala",
+                    PageSlug = "irina-xyz"
+
+                });
+
+            }
+
+            if (!dbContext.Talks.Any())
+            {
+                dbContext.Talks.Add(new Talk()
+                {
+                    Description = "lalaa",
+                    Title = "GraphQL",
+                    SpeakerId = dbContext.Speakers.First().Id
+                });
+                dbContext.Talks.Add(new Talk()
+                {
+                    Description = "lalaa",
+                    Title = "Yet another graphql talk 2",
+                    SpeakerId = dbContext.Speakers.First().Id
+                });
 
             }
         }
