34b0395fef869c9ce2b4d983423f00069c3b7756
irina.scurtu <irina.scurtu@fortech.ro>
2019-03-07 16:47:48 +0200

    added initial migration

--- GraphQL.Api/GraphQL.Api/Data/Entities/Speaker.cs
+++ GraphQL.Api/GraphQL.Api/Data/Entities/Speaker.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GraphQL.Api.Data.Entities
 {
@@ -41,5 +37,7 @@ namespace GraphQL.Api.Data.Entities
         [Required]
         public string Description { get; set; }
         public string PageSlug { get; set; }
+
+        public List<Talk> Talks { get; set; }
     }
 }
---
+++ GraphQL.Api/GraphQL.Api/Data/Migrations/20190307144258_InitialCreate.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GraphQL.Api.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Speakers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    Position = table.Column<string>(maxLength: 50, nullable: false),
+                    Website = table.Column<string>(nullable: true),
+                    Facebook = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    LinkedIn = table.Column<string>(nullable: false),
+                    Skype = table.Column<string>(nullable: true),
+                    GitHub = table.Column<string>(nullable: false),
+                    Twitter = table.Column<string>(nullable: true),
+                    CompanyName = table.Column<string>(nullable: true),
+                    CompanyWebsite = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: false),
+                    PageSlug = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speakers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Talks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SpeakerId = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Talks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Talks_Speakers_SpeakerId",
+                        column: x => x.SpeakerId,
+                        principalTable: "Speakers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Talks_SpeakerId",
+                table: "Talks",
+                column: "SpeakerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Talks");
+
+            migrationBuilder.DropTable(
+                name: "Speakers");
+        }
+    }
+}
