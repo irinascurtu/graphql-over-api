faf29982e7e62761399e800db156858bc1bd1352
irina.scurtu <irina.scurtu@fortech.ro>
2019-03-15 14:14:56 +0200

    added feedbacks

---
+++ GraphQL.Api/Feedbacks.Api/Data/FeedbacksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Feedbacks.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Feedbacks.Api.Data
+{
+    public class FeedbacksDbContext : DbContext
+    {
+        public FeedbacksDbContext(DbContextOptions<FeedbacksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Feedback> Feedbacks { get; set; }
+    }
+}
+
---
+++ GraphQL.Api/Feedbacks.Api/Data/Migrations/20190315120214_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Feedbacks.Api.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feedbacks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Content = table.Column<int>(nullable: false),
+                    Delivery = table.Column<int>(nullable: false),
+                    Comments = table.Column<string>(nullable: true),
+                    TalkId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedbacks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feedbacks");
+        }
+    }
+}
